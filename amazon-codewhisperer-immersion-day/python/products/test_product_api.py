f1f2eb7be91bd83f99bab7debe344ca2b19b1e7d
jpcp0614 <jpcp0614@gmail.com>
2024-01-06 17:17:34 -0300

    Desafio - API de Produtos -> Cria mais um teste

--- amazon-codewhisperer-immersion-day/python/products/test_product_api.py
+++ amazon-codewhisperer-immersion-day/python/products/test_product_api.py
@@ -1,14 +1,34 @@
 import product_api
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 
 class TestProductApi(TestCase):
-    
+
     # Patch get_products and test lambda_handler returns a single record
     @patch('product_api.get_products')
     def test_lambda_handler_single(self, mock_get_products):
-        mock_get_products.return_value = [{'id': '1', 'name': 'test', 'description': 'test'}]
+        mock_get_products.return_value = [
+            {'id': '1', 'name': 'test', 'description': 'test'}
+            ]
         response = product_api.lambda_handler(None, None)
         self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(response['body'], '[{"id": "1", "name": "test", "description": "test"}]')
+        self.assertEqual(
+            response['body'],
+            '[{"id": "1", "name": "test", "description": "test"}]'
+            )
+
+    # Patch get_products and test lambda_handler returns a multiple records
+    @patch('product_api.get_products')
+    def test_lambda_handler_multiple(self, mock_get_products):
+        mock_get_products.return_value = [
+            {'id': '1', 'name': 'test', 'description': 'test'},
+            {'id': '2', 'name': 'test2', 'description': 'test2'}
+            ]
+        response = product_api.lambda_handler(None, None)
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(
+            response['body'],
+            '[{"id": "1", "name": "test", "description": "test"}, '
+            '{"id": "2", "name": "test2", "description": "test2"}]'
+            )
