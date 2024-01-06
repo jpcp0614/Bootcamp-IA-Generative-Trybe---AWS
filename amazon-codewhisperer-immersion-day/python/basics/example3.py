e2aa75f4b1da134fd83922b82e5088c401d001c4
jpcp0614 <jpcp0614@gmail.com>
2024-01-06 12:29:17 -0300

    Alt+C -> Criou a função a partir de comentários

--- amazon-codewhisperer-immersion-day/python/basics/example3.py
+++ amazon-codewhisperer-immersion-day/python/basics/example3.py
@@ -1,3 +1,18 @@
 # Parse a CSV string of songs and return a list of dictionaries
 # Fields include artist, album, title and year
 # Ignore lines starting with
+def parse_songs(csv_string):
+    songs = []
+    for line in csv_string.splitlines():
+        if line.startswith("#"):
+            continue
+        fields = line.split(",")
+        songs.append(
+            {
+                "artist": fields[0],
+                "album": fields[1],
+                "title": fields[2],
+                "year": fields[3],
+            }
+        )
+    return songs
