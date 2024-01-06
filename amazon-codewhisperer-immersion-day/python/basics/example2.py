2640f50defbc840f3fba8f9b7a1aad40057d7d77
jpcp0614 <jpcp0614@gmail.com>
2024-01-06 12:24:38 -0300

    Alt+C -> Criou a função

--- amazon-codewhisperer-immersion-day/python/basics/example2.py
+++ amazon-codewhisperer-immersion-day/python/basics/example2.py
@@ -1,1 +1,16 @@
 # Function to upload a file to an S3 bucket
+import boto3
+import logging
+
+
+def upload_file(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = file_name
+
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
