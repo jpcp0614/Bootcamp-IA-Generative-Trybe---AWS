ab3c386ddbd4ba928108676d9c7e7c430283107d
jpcp0614 <jpcp0614@gmail.com>
2024-01-06 13:16:06 -0300

    Contexto adicional

--- amazon-codewhisperer-immersion-day/python/prompts/example2a.py
+++ amazon-codewhisperer-immersion-day/python/prompts/example2a.py
@@ -1,1 +1,40 @@
 # Create a function to create a DynamoDB table
+import boto3
+from botocore.exceptions import ClientError
+
+
+def create_table(table_name, partition_key, sort_key):
+    dynamodb = boto3.resource('dynamodb')
+
+    try:
+        table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': partition_key,
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': sort_key,
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': partition_key,
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': sort_key,
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+        return table
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None
